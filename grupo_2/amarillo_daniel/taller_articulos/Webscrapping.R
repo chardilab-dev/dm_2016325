0a1e4295ad67d97bf4aaa85454ee9d1c6fa528fb
Damarillo <102869725+Damarillo@users.noreply.github.com>
2026-05-08 13:59:12 -0500

    Extracción de citas y detalles del articulo.

--- grupo_2/amarillo_daniel/taller_articulos/Webscrapping.R
+++ grupo_2/amarillo_daniel/taller_articulos/Webscrapping.R
@@ -19,7 +19,8 @@ url <- "https://www.akjournals.com/view/journals/2006/14/1/2006.14.issue-1.xml"
 issues_url <- c("https://www.akjournals.com/view/journals/2006/14/1/2006.14.issue-1.xml"
                 ,"https://www.akjournals.com/view/journals/2006/14/2/2006.14.issue-2.xml"
                 ,"https://www.akjournals.com/view/journals/2006/14/3/2006.14.issue-3.xml"
-                ,"https://www.akjournals.com/view/journals/2006/14/4/2006.14.issue-4.xml")
+                ,"https://www.akjournals.com/view/journals/2006/14/4/2006.14.issue-4.xml"
+                ,"https://www.akjournals.com/view/journals/2006/15/1/2006.15.issue-1.xml")
 
 crear_nodo <- function(url){
 # Definimos un user-agent similar al de un navegador real
@@ -32,14 +33,14 @@ user_agent_navegador <- paste(
 # Construimos la solicitud HTTP y añadimos encabezados para 
 # que parezca una visita normal
 
-respuesta <- request(url) %>%
-  req_user_agent(user_agent_navegador) %>%
-  req_headers(`accept-language` = "en-US,en;q=0.9") %>%
+respuesta <- request(url) |>
+  req_user_agent(user_agent_navegador) |>
+  req_headers(`accept-language` = "en-US,en;q=0.9") |>
   req_perform()
 
 # Convertimos el cuerpo de la respuesta en un documento HTML
 # analizable con rvest/xml2
-nodo <- respuesta %>%
+nodo <- respuesta |>
   resp_body_html()
 nodo
 }
@@ -49,8 +50,8 @@ nodo
 
 extraccion_general <- function(nodo) {
   # Extraemos el títulos de los articulos
-  titulo <- nodo %>%
-    html_elements("[data-testid='block-primitivetitle']") %>%
+  titulo <- nodo |>
+    html_elements("[data-testid='block-primitivetitle']") |>
     html_text2()
   
   issue <- titulo[length(titulo)]
@@ -79,11 +80,48 @@ for (i in issues_url) {
 doi_prueba <- generalities$doi[1]
 pagina <- crear_nodo(doi_prueba)
 
-autores_1 <- pagina |> 
+ #EXTRACCIÓN DOI (LLAVE EN COMUN CON GENERALITIES)
+doi <- pagina |> 
+  html_elements( " a,[target='_blank'], .c-Button--link") |>
+  html_text()
+doi <- doi[grep(pattern="https://doi*",doi)][1]
+
+ #AUTORES 
+autores_ex <- pagina |> 
   html_elements("[data-testid='author-name']") |>
   html_text()
+autores <- paste(autores_ex,collapse = ",")
+ 
+ #ABSTRACT
+ raw_abstract <- pagina |> html_elements(".abstract.border-bottom.border-bottom-solid.border-bottom-medium") |>
+   html_text2()
+ a<- unlist(strsplit(raw_abstract, split = "\n"))
+ Backround_and_aims <- a[4]
+ Methods <- a[8]
+ Results <- a[12]
+ Conclusion <- a[16]
 
-generalities["Autores"] <- autores_1
-doi <- doi[grep(pattern="https://doi*",doi)]
-as.data.frame(t(autores))
-autores_1[1]autoautores_1res_1[1]
+ fecha <- pagina |> 
+   html_elements(".printpubdate.c-List__items") |>
+   html_text2() |>
+   str_remove_all("\t|\n") |>
+   str_remove("^[^0-9]+")
+ 
+ #URL 
+ URL <- pagina |> 
+   html_elements('[rel="canonical"], href')|>
+   html_attr("href")
+ 
+ ref_ex <- pagina |> 
+   html_elements(".citationText.text-body1") |>
+   html_text()
+ #SEPARADOR PROPIO /--/
+ referencias <- paste(ref_ex,collapse = "/--/")
+ 
+ 
+ # CITAS
+ dyn_cit = read_html_live(URL)
+ dyn_cit$click("[data-tab-id='citedBy-30734']")
+ dyn_cit |> html_elements("#citedByWidget") |>
+   html_children("p")
+ on.exit(dyn_cit$session$close())
